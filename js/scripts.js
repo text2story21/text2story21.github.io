1ad549655921e3d2bd474586025e2d7a22de27bf
01alex <xanomcr@gmail.com>
2020-12-16 16:08:08 +0000

    fix browser console errors

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ jQuery(function ($) {
     // PreLoader
     // --------------------------------------------------------------------
 
-    (function () {
-        $('#preloader').delay(200).fadeOut('slow');
-    }());
+    // (function () {
+    //     $('#preloader').delay(200).fadeOut('slow');
+    // }());
 
     // --------------------------------------------------------------------
     // One Page Navigation
@@ -55,38 +55,38 @@ jQuery(function ($) {
     // Map
     // --------------------------------------------------------------------
 
-    (function () {
-        var latitude  = 50.944512; //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-        var longitude = 6.9503217;
-        var map_zoom  = 14;
+    // (function () {
+    //     var latitude  = 50.944512; //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+    //     var longitude = 6.9503217;
+    //     var map_zoom  = 14;
 
-        var platform = new H.service.Platform({
-            'app_id': 'IwGH4C4l25qnAx4Ysqsb',
-            'app_code': 'j3LDYCZGo1TBuCP3lxwEqw'
-          });
+    //     var platform = new H.service.Platform({
+    //         'app_id': 'IwGH4C4l25qnAx4Ysqsb',
+    //         'app_code': 'j3LDYCZGo1TBuCP3lxwEqw'
+    //       });
 
-          // Obtain the default map types from the platform object:
-            var defaultLayers = platform.createDefaultLayers();
+    //       // Obtain the default map types from the platform object:
+    //         var defaultLayers = platform.createDefaultLayers();
 
-            // Instantiate (and display) a map object:
-            var map = new H.Map(
-            document.getElementById('mapContainer'),
-            defaultLayers.normal.map);
+    //         // Instantiate (and display) a map object:
+    //         var map = new H.Map(
+    //         document.getElementById('mapContainer'),
+    //         defaultLayers.normal.map);
 
-            // center address
-            map.setCenter({lat:latitude, lng:longitude});
-            map.setZoom(map_zoom);
+    //         // center address
+    //         map.setCenter({lat:latitude, lng:longitude});
+    //         map.setZoom(map_zoom);
 
-            // add marker
-            map.addObject(new H.map.Marker({lat:latitude, lng:longitude}));
+    //         // add marker
+    //         map.addObject(new H.map.Marker({lat:latitude, lng:longitude}));
 
-            //Step 3: make the map interactive
-            // MapEvents enables the event system
-            // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-            var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-            // Create the default UI components
-            var ui = H.ui.UI.createDefault(map, defaultLayers);
+    //         //Step 3: make the map interactive
+    //         // MapEvents enables the event system
+    //         // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+    //         var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    //         // Create the default UI components
+    //         var ui = H.ui.UI.createDefault(map, defaultLayers);
 
-    }());
+    // }());
 
 }); // JQuery end
